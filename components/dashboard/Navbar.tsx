a74b96a2c920fa6d609b97a0ef02a9d95845e5dd
stormymayday <stormymayday7@gmail.com>
2024-12-13 17:25:37 -0800

    Navbar

--- components/dashboard/Navbar.tsx
+++ components/dashboard/Navbar.tsx
@@ -1,4 +1,18 @@
+import LinksDropdown from "@/components/dashboard/LinksDropdown";
+import UserButton from "@/components/auth/UserButton";
+import { ModeToggle } from "@/components/mode-toggle";
+
 function Navbar() {
-    return <h2 className="text-4xl">Navbar</h2>;
+    return (
+        <nav className="bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between">
+            <div>
+                <LinksDropdown />
+            </div>
+            <div className="flex items-center gap-x-4">
+                <ModeToggle />
+                <UserButton />
+            </div>
+        </nav>
+    );
 }
 export default Navbar;
