a78e80e1241bd2073d564276e2f79647c534ca8a
stormymayday <stormymayday7@gmail.com>
2024-12-13 22:37:25 -0800

    react query

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,27 +3,29 @@ import { auth } from "@/auth";
 
 import Navbar from "@/components/dashboard/Navbar";
 import Sidebar from "@/components/dashboard/Sidebar";
-// import LinksDropdown from "@/components/dashboard/LinksDropdown";
+import ReactQueryProvider from "@/providers/react-query-provider";
 
 async function DashboardLayout({ children }: { children: React.ReactNode }) {
     const session = await auth();
     return (
         <SessionProvider session={session}>
-            <main className="grid lg:grid-cols-5">
-                {/* first column is hidden on small screen */}
-                <div className="hidden lg:block lg:col-span-1 lg:min-h-screen">
-                    <Sidebar />
-                </div>
-
-                {/* second column: hide the dropdown on big screen */}
-                <div className="lg:col-span-4">
-                    <Navbar />
-
-                    <div className="py-16 px-4 sm:px-8 lg:px-16">
-                        {children}
+            <ReactQueryProvider>
+                <main className="grid lg:grid-cols-5">
+                    {/* first column is hidden on small screen */}
+                    <div className="hidden lg:block lg:col-span-1 lg:min-h-screen">
+                        <Sidebar />
                     </div>
-                </div>
-            </main>
+
+                    {/* second column: hide the dropdown on big screen */}
+                    <div className="lg:col-span-4">
+                        <Navbar />
+
+                        <div className="py-16 px-4 sm:px-8 lg:px-16">
+                            {children}
+                        </div>
+                    </div>
+                </main>
+            </ReactQueryProvider>
         </SessionProvider>
     );
 }
