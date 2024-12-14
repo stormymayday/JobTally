2c60d1adda8e85bfd77a4a7497867bd5d92f581b
stormymayday <stormymayday7@gmail.com>
2024-12-13 16:56:35 -0800

    DashboardLayout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,29 @@
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
 
+import Navbar from "@/components/dashboard/Navbar";
+import Sidebar from "@/components/dashboard/Sidebar";
+// import LinksDropdown from "@/components/dashboard/LinksDropdown";
+
 async function DashboardLayout({ children }: { children: React.ReactNode }) {
     const session = await auth();
     return (
         <SessionProvider session={session}>
-            <main>{children}</main>
+            <main className="grid lg:grid-cols-5">
+                {/* first column is hidden on small screen */}
+                <div className="hidden lg:block lg:col-span-1 lg:min-h-screen">
+                    <Sidebar />
+                </div>
+
+                {/* second column: hide the dropdown on big screen */}
+                <div className="lg:col-span-4">
+                    <Navbar />
+
+                    <div className="py-16 px-4 sm:px-8 lg:px-16">
+                        {children}
+                    </div>
+                </div>
+            </main>
         </SessionProvider>
     );
 }
