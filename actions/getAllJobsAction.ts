9877a7966850162a6eaec977ee105d2abaa16c58
stormymayday <stormymayday7@gmail.com>
2024-12-14 13:42:45 -0800

    getAllJobsAction

---
+++ actions/getAllJobsAction.ts
@@ -0,0 +1,107 @@
+"use server";
+
+// import * as z from "zod";
+import { db } from "@/lib/db";
+// import { createAndEditJobSchema } from "@/schemas";
+import { Job, Prisma } from "@prisma/client";
+import { currentUser } from "@/utils/server-current-user/currentUser";
+
+type getAllJobsActionType = {
+    searchTerm?: string;
+    jobStatus?: string;
+    page?: number;
+    limit?: number;
+};
+
+export async function getAllJobsAction({
+    searchTerm = "",
+    jobStatus = "all",
+    page = 1,
+    limit = 10,
+}: getAllJobsActionType): Promise<{
+    jobs: Job[];
+    count: number;
+    page: number;
+    totalPages: number;
+    successMessage: string;
+    errorMessage: string;
+}> {
+    const user = await currentUser();
+
+    if (!user) {
+        return {
+            jobs: [],
+            count: 0,
+            page: 1,
+            totalPages: 0,
+            successMessage: "",
+            errorMessage: "Unauthorized",
+        };
+    }
+
+    console.log(page, limit);
+
+    try {
+        // Constructing where clause:
+        // 1. attaching the use ID
+        let whereClause: Prisma.JobWhereInput = {
+            userId: user.id,
+        };
+
+        // 2. If the searchTerm was provided, check if position or company contains it
+        if (searchTerm) {
+            whereClause = {
+                ...whereClause,
+                OR: [
+                    {
+                        position: {
+                            contains: searchTerm,
+                            mode: "insensitive",
+                        },
+                    },
+                    {
+                        company: {
+                            contains: searchTerm,
+                            mode: "insensitive",
+                        },
+                    },
+                ],
+            };
+        }
+
+        // 3. Checking job status if it was provided and is not equal to 'all'
+        if (jobStatus && jobStatus !== "all") {
+            whereClause = {
+                ...whereClause,
+                status: jobStatus,
+            };
+        }
+
+        const jobs: Job[] = await db.job.findMany({
+            where: whereClause,
+            orderBy: {
+                createdAt: "desc",
+            },
+        });
+
+        // return { jobs: jobs, count: 0, page: 1, totalPages: 0 };
+        return {
+            jobs: jobs,
+            count: 0,
+            page: 1,
+            totalPages: 0,
+            successMessage: "Jobs fetched",
+            errorMessage: "",
+        };
+    } catch {
+        // return { jobs: [], count: 0, page: 0, totalPages: 0 };
+        return {
+            jobs: [],
+            count: 0,
+            page: 1,
+            totalPages: 0,
+            successMessage: "",
+            errorMessage: "Failed to fetch the jobs",
+        };
+    }
+}
--- app/(dashboard)/jobs/page.tsx
+++ app/(dashboard)/jobs/page.tsx
@@ -1,4 +1,10 @@
-function JobsPage() {
+import { getAllJobsAction } from "@/actions/getAllJobsAction";
+
+async function JobsPage() {
+    const result = await getAllJobsAction({});
+
+    console.log(result);
+
     return <h1 className="text-4xl">Jobs Page</h1>;
 }
 export default JobsPage;
