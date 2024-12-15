183088f0fb8c1be19d7b11f3e76dbe28632bf8ab
stormymayday <stormymayday7@gmail.com>
2024-12-14 21:44:10 -0800

    JobInfo

--- components/dashboard/JobCard.tsx
+++ components/dashboard/JobCard.tsx
@@ -1,6 +1,6 @@
 import { Job } from "@prisma/client";
 import Link from "next/link";
-// import { MapPin, Briefcase, CalendarDays, RadioTower } from "lucide-react";
+import { MapPin, Briefcase, CalendarDays, RadioTower } from "lucide-react";
 import {
     Card,
     CardContent,
@@ -11,16 +11,16 @@ import {
 } from "@/components/ui/Card";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/Button";
-// import { Badge } from "@/components/ui/Badge";
+import { Badge } from "@/components/ui/Badge";
 
-// import JobInfo from "@/components/dashboard/JobInfo";
+import JobInfo from "@/components/dashboard/JobInfo";
 import DeleteJobButton from "@/components/dashboard/DeleteJobButton";
 
 interface JobCardProps {
     job: Job;
 }
 function JobCard({ job }: JobCardProps) {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // Formatting the date
     const date = new Date(job.createdAt).toLocaleDateString();
 
     return (
@@ -30,7 +30,17 @@ function JobCard({ job }: JobCardProps) {
                 <CardDescription>{job.company}</CardDescription>
             </CardHeader>
             <Separator />
-            <CardContent>{/* card info */}</CardContent>
+            <CardContent className="mt-4 grid grid-cols-2 gap-4">
+                <JobInfo icon={<Briefcase />} text={job.mode} />
+                <JobInfo icon={<MapPin />} text={job.location} />
+                <JobInfo icon={<CalendarDays />} text={date} />
+                <Badge className="w-32  justify-center">
+                    <JobInfo
+                        icon={<RadioTower className="w-4 h-4" />}
+                        text={job.status}
+                    />
+                </Badge>
+            </CardContent>
             <CardFooter className="flex gap-4">
                 <Button asChild size="sm">
                     <Link href={`/jobs/${job.id}`}>edit</Link>
--- components/dashboard/JobInfo.tsx
+++ components/dashboard/JobInfo.tsx
@@ -1,4 +1,14 @@
-function JobInfo() {
-    return <h1 className="text-4xl">Job Info</h1>;
+interface JobInfoProps {
+    icon: React.ReactNode;
+    text: string;
+}
+
+function JobInfo({ icon, text }: JobInfoProps) {
+    return (
+        <div className="flex gap-x-2 items-center">
+            {icon}
+            {text}
+        </div>
+    );
 }
 export default JobInfo;
